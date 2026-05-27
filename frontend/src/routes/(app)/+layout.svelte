967f8cfafb0c0d8f270c016d13c38bf0fd06b833
Jonas Berghammer <berghammerj22013@sus.htl-grieskirchen.at>
2026-05-27 11:30:24 +0200

    load user in app layout

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -1,12 +1,20 @@
 <script>
 	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 	import { isLoggedIn } from '$lib/auth.js';
+	import { userStore } from '$lib/userStore.svelte.js';
 	import Navbar from '$lib/components/Navbar.svelte';
 
 	let { children } = $props();
 
 	if (browser && !isLoggedIn()) goto('/login');
+
+	onMount(async () => {
+		if (isLoggedIn() && !userStore.user) {
+			await userStore.load();
+		}
+	});
 </script>
 
 <div class="pb-16">
